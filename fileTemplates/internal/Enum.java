2be557b6cfd80f0cfc879c2337a05c3dad2651d0
gyoomi <gyoomi0709@foxmail.com>
2018-11-29 17:35:19 +0800

    IU-2018.3 <Administrator@A3OZF9V1REX6UR5 Create IntelliLang.xml, print.xml, project.default.xml, diff.xml, Enum.java, Default copy.xml, Interface.java, yiiguxing.translation.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, File Header.java, keymap.xml, _@user_Darcula.icls, mavenVersion.xml, Default.xml, editor.codeinsight.xml, grepConsole.xml, editor.xml, markdown_navigator.shared.xml, debugger.xml, MyGroup.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, ide-features-trainer.xml, CodeGlance.xml, lombok-plugin.xml, other.xml, ui.lnf.xml, filetypes.xml, projectView.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, VisualVMLauncher.xml, databaseDrivers.xml, github.xml, templates.xml, AnnotationType.java, laf.xml, KeyPromoterXStatistic.xml, Eclipse.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, nbmindmapsettings.xml

---
+++ fileTemplates/internal/Enum.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright © ${YEAR}, TaoDing
+ *
+ * All Rights Reserved.
+ */
+
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * 类功能描述
+ *
+ * @author	Leon
+ * @version	  ${DATE} ${TIME}
+ */
+public enum ${NAME} {
+}
